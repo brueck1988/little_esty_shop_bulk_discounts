600b918e59f7ac9275940da2237d8f83740b8395
brueck1988 <brueck1988@gmail.com>
2021-04-25 21:43:32 -0600

    User story 7 progress

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,17 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: [:cancelled, :in_progress, :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def total_revenue_with_bulk_discounts
+    require "pry";binding.pry
+    Invoice.joins(:bulk_discounts).where("bulk_discounts.quantity_threshold <= invoice_items.quantity").sum("(invoice_items.unit_price * invoice_items.quantity * bulk_discounts.percentage_discount / 100")
+    #Invoice.joins(:bulk_discounts).where("invoice.status = 2 AND bulk_discounts.quantity_threshold <= invoice_items.quantity").sum("(invoice_items.unit_price * invoice_items.quantity) * (bulk_discounts.discount/100)")
   end
 end
